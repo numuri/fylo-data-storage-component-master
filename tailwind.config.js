e0793d045770d0de166f2069be172680f99a0f8b
Nur Muhammad Rizky <nrmrizky5@gmail.com>
2023-07-17 02:09:42 +0700

    make storage component desain by frontend mentor

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        paleBale: "hsl(243, 100%, 93%)",
+        grayishBlue: "hsl(229, 7%, 55%)",
+        darkBlue: "hsl(228, 56%, 26%)",
+        veryDarkBlue: "hsl(229, 57%, 11%)",
+        gradientOne: "hsl(6, 100%, 80%)",
+        gradientTwo: "hsl(335, 100%, 65%)",
+      },
+    },
+  },
+  plugins: [],
+};
